a4f3e212c3c7e313c87eb101b962082bef658d75
Ulysses Levy <levy.ulysses@gmail.com>
2013-06-27 18:03:31 -0700

    added a quick comment on the main activity

--- src/org/aschyiel/peaches/Main.java
+++ src/org/aschyiel/peaches/Main.java
@@ -3,6 +3,9 @@ package org.aschyiel.peaches;
 import android.app.Activity;
 import android.os.Bundle;
 
+/**
+* This is the main acitivity.
+*/
 public class Main extends Activity
 {
     /** Called when the activity is first created. */
